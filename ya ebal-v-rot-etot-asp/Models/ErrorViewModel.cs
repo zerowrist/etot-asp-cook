12f9499e5e1f3819a4ec73d71f2e4824efe12dd5
zerowrist <76410578+zerowrist@users.noreply.github.com>
2020-12-30 00:09:26 +0300

    cooking site
    cooking site-blog

---
+++ ya ebal-v-rot-etot-asp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ya_ebal_v_rot_etot_asp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
